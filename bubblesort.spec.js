f0d5191b8701919d509a656630e486df10f6bb69
Samir Thakral <samirthakral@icloud.com>
2018-04-23 11:11:31 -0500

    Wrote test specs

---
+++ bubblesort.spec.js
@@ -0,0 +1,17 @@
+describe('bubblesort.js', () => {
+
+  describe('Bubble Sort', function(){
+    it('handles an empty array', function(){
+      expect( bubbleSort([]) ).toEqual( [] );
+    });
+
+    it('handles a single item', function(){
+      expect(bubbleSort([5])).toEqual([5]);
+    });
+
+    it('handles multiple items', function(){
+      let arr = [3,14,1,8,2,10];
+      expect(bubbleSort(arr)).toEqual([1,2,3,8,10,14]);
+    });
+  })
+})
