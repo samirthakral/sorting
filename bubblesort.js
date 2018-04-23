15015bea8a107e099306998b3d96d573d0a5680a
Samir Thakral <samirthakral@icloud.com>
2018-04-23 13:52:01 -0500

    Finished w/o bonus

--- bubblesort.js
+++ bubblesort.js
@@ -1,26 +1,17 @@
-var firstnumber = 0;
-var secondnumber = 0;
-
 function bubbleSort(array) {
   if (array.length <= 1) return array;
   for (let i = 1; i < array.length - 1; i++) {
     for (let j = 0; j < array.length - i; j++) {
       if (array[j] > array[j + 1]) {
-        //   let temp = array[j];
-        //   array[j] = array[j+1];
-        //   array[j+1] = temp;
-        swap(array[j], array[j + 1]);
-        array[j] = firstnumber;
-        array[j + 1] = secondnumber;
+        let temp = swap(array[j], array[j + 1]);
+        array[j] = temp[0];
+        array[j + 1] = temp[1];
       }
-      console.log(array);
     }
   }
   return array;
 }
 
-function swap(num1, num2) {
-  firstnumber = num2;
-  secondnumber = num1;
-
+function swap(num1,num2) {
+  return [num2,num1]
 }
